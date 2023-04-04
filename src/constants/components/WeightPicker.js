bd0738a2ffabddcdf3aa666723c667ba3f53107b
Zac Messinger <zdmessinger@gmail.com>
2023-04-03 21:05:36 -0400

    Added floats

--- src/constants/components/WeightPicker.js
+++ src/constants/components/WeightPicker.js
@@ -7,7 +7,7 @@ function WeightPicker({ handleWeightChange, weight }) {
 	const [int, setInt] = useState(weight.toString().indexOf(".") > -1 ? parseInt(weight.toString().split(".")[0]) : weight);
 	const [float, setFloat] = useState(weight.toString().indexOf(".") > -1 ? `.${weight.toString().split(".")[1]}` : 0);
 	const intRange = new Array(500 - 50).fill().map((d, i) => i + 50);
-	const floatRange = new Array(9 - 0).fill().map((d, i) => i + 1);
+	const floatRange = new Array(9 - 0).fill().map((d, i) => i + 0);
 
 	const intItems = intRange.map((i, v) => {
 		return <Picker.Item label={`${i}`} value={i} key={i} style={{ height: 10 }} />;
@@ -15,7 +15,7 @@ function WeightPicker({ handleWeightChange, weight }) {
 
 	const floatItems = floatRange.map((i, v) => {
 		let float = (i / 10).toString().substring(1);
-		return <Picker.Item label={`${float} lbs`} value={float} key={float} />;
+		return <Picker.Item label={`.${i} lbs`} value={float} key={float} />;
 	});
 
 	const handleFloatChange = (val) => {
