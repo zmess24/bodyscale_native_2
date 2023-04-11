a485fd99679a4bb08d68f3db6186985f1dea81c2
Zac Messinger <zdmessinger@gmail.com>
2023-04-10 19:52:39 -0700

    Added correct date parsing

--- src/screens/ChartScreen/components/charts/CurrentMonthChart.js
+++ src/screens/ChartScreen/components/charts/CurrentMonthChart.js
@@ -5,13 +5,18 @@ import moment from "moment";
 function CurrentMonthChart({ userData }) {
 	let yMax = 0;
 	let yMin = 1000;
-	console.log("Current month is:", moment().month());
+	let firstOfMonth = Date.parse(moment().date(1).format("YYYY-MM-DD"));
+	let endOfMonth = Date.parse(moment().date(1).endOf("month").format("YYYY-MM-DD"));
 
-	let chartData = userData.entries.map((w, i) => {
-		if (parseFloat(w.average) < yMin) yMin = parseFloat(w.average);
-		if (parseFloat(w.average) > yMax) yMax = parseFloat(w.average);
-		return { date: new Date(moment(w.startDate)), average: parseFloat(w.average), key: i };
-	});
+	let chartData = userData.entries
+		.filter((w, i) => {
+			if (firstOfMonth < new Date(moment(w.startDate)) && new Date(moment(w.startDate)) < endOfMonth) return w;
+		})
+		.map((w, i) => {
+			if (parseFloat(w.average) < yMin) yMin = parseFloat(w.average);
+			if (parseFloat(w.average) > yMax) yMax = parseFloat(w.average);
+			return { date: new Date(moment(w.startDate)), average: parseFloat(w.average), key: i };
+		});
 
 	return <AreaChart chartData={chartData} yMax={yMax} yMin={yMin} />;
 }
