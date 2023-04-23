7250898d514361c418e62e247066896844ef769f
Zac Messinger <zdmessinger@gmail.com>
2023-04-23 15:43:50 -0700

    added sorting on week screen

--- src/screens/ChartScreen/components/charts/CurrentWeekChart.js
+++ src/screens/ChartScreen/components/charts/CurrentWeekChart.js
@@ -6,11 +6,18 @@ function CurrentWeekChart({ userData }) {
 	let yMax = 0;
 	let yMin = 1000;
 
-	let chartData = userData.entries.at(-1).data.map((d, i) => {
-		if (parseFloat(d.weight) < yMin) yMin = parseFloat(d.weight);
-		if (parseFloat(d.weight) > yMax) yMax = parseFloat(d.weight);
-		return { x: moment(d.date).format("MM/DD"), y: parseFloat(d.weight), key: i };
-	});
+	console.log(userData.entries);
+
+	let chartData = userData.entries
+		.at(0)
+		.data.sort((a, b) => new Date(a.date) - new Date(b.date))
+		.map((d, i) => {
+			if (parseFloat(d.weight) < yMin) yMin = parseFloat(d.weight);
+			if (parseFloat(d.weight) > yMax) yMax = parseFloat(d.weight);
+			return { x: moment(d.date).format("MM/DD"), y: parseFloat(d.weight), key: i };
+		});
+
+	console.log("WEEK SCREEN", chartData);
 
 	return <AreaChart chartData={chartData} yMax={yMax} yMin={yMin} />;
 }
