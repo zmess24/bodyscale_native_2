4fde1eb82e8eefb1edfba5b4dccdd6a7429132c8
Zac Messinger <zdmessinger@gmail.com>
2023-05-27 17:55:39 -0700

    adjusted background color on WeekRow

--- src/screens/HistoryScreen/components/WeekRow.js
+++ src/screens/HistoryScreen/components/WeekRow.js
@@ -8,8 +8,8 @@ const WeekRow = ({ data }) => (
 	<View>
 		<View style={tw.style("flex flex-row justify-between items-center rounded py-2 px-3 bg-gray-100")}>
 			<View style={tw.style("flex flex-row items-center")}>
-				<MaterialCommunityIcons name="scale-bathroom" size={18} color="gray" />
-				<Text style={tw.style("pl-1 text-sm font-semibold")}>{moment(data.endDate).format("MMMM Do, YYYY")}</Text>
+				<MaterialCommunityIcons name="scale-bathroom" size={16} color="gray" />
+				<Text style={tw.style("pl-2 text-sm font-semibold")}>{moment(data.endDate).format("MMMM Do, YYYY")}</Text>
 			</View>
 			<View style={tw.style("flex flex-col items-end")}>
 				<Text style={tw.style("text-sm font-semibold")}>{data.average} lbs</Text>
