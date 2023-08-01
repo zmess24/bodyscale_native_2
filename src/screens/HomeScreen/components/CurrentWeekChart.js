9ace2a1c5599b066494a61fff2f39a827e171b38
Zac Messinger <zdmessinger@gmail.com>
2023-08-01 11:14:49 -0400

    refactored week chart into single component

--- src/screens/HomeScreen/components/CurrentWeekChart.js
+++ src/screens/HomeScreen/components/CurrentWeekChart.js
@@ -1,30 +1,62 @@
-import React from "react";
-import AreaChart from "./AreaChart";
+import React, { useState } from "react";
+import { Dimensions } from "react-native";
 import moment from "moment";
-import { Text } from "react-native";
+import { VictoryChart, VictoryArea, VictoryScatter, VictoryAxis, VictoryTheme } from "victory-native";
+import { colorTheme } from "../../../constants/styles";
 
-function CurrentWeekChart({ userData }) {
-	let yMax = 0;
-	let yMin = 1000;
+function CurrentWeekChart({ startDate, endDate, data }) {
+	const windowWidth = Dimensions.get("window").width;
+	const windowHeight = Dimensions.get("window").height * 0.25;
+	let yMax = 215;
+	let yMin = 200;
 
-	console.log(userData.entries);
+	let enumerateDaysBetweenDates = (startDate, endDate) => {
+		let dates = [];
 
-	let chartData = userData.entries
-		.at(0)
-		.data.sort((a, b) => new Date(a.date) - new Date(b.date))
-		.map((d, i) => {
-			if (parseFloat(d.weight) < yMin) yMin = parseFloat(d.weight);
-			if (parseFloat(d.weight) > yMax) yMax = parseFloat(d.weight);
-			return { x: moment(d.date).format("MM/DD"), y: parseFloat(d.weight), key: i };
+		while (moment(startDate) <= moment(endDate)) {
+			dates.push(startDate);
+			startDate = moment(startDate).add(1, "days").format("YYYY-MM-DD");
+		}
+
+		return dates;
+	};
+
+	let chartData = data
+		.sort((a, b) => new Date(a.date) - new Date(b.date))
+		.map((entry, i) => {
+			if (parseFloat(entry.weight) < yMin) yMin = parseFloat(entry.weight);
+			if (parseFloat(entry.weight) > yMax) yMax = parseFloat(entry.weight);
+			return { x: moment(entry.date).format("YYYY-MM-DD"), y: parseFloat(entry.weight), key: i };
 		});
 
-	console.log("RENDER CHART", chartData.length >= 2);
-
-	if (chartData.length >= 2) {
-		return <AreaChart chartData={chartData} yMax={yMax} yMin={yMin} />;
-	} else {
-		return <Text style={tw.style("text-4xl font-bold tracking-tight")}>Not Enough Data</Text>;
-	}
+	return (
+		<VictoryChart width={windowWidth} height={windowHeight} theme={VictoryTheme.material} domain={{ y: [yMin - 5, yMax + 5] }}>
+			<VictoryAxis
+				dependentAxis
+				maxDomain={{ y: 5 }}
+				style={{
+					grid: { stroke: "white", strokeWidth: 0 },
+					ticks: { stroke: "white", strokeWidth: 0 },
+				}}
+			/>
+			<VictoryAxis
+				tickValues={enumerateDaysBetweenDates(startDate, endDate)}
+				maxDomain={{ x: 7 }}
+				style={{
+					grid: { stroke: "lightgrey", strokeWidth: 1 },
+					ticks: { stroke: "white", strokeWidth: 0 },
+				}}
+				tickFormat={(t) => `${moment(t).format("ddd")}\n${moment(t).format("D")}`}
+			/>
+			<VictoryArea
+				interpolation="monotoneX"
+				style={{ data: { stroke: colorTheme.accent, strokeWidth: 2, fill: colorTheme.accent, fillOpacity: 0.25 } }}
+				scale={{ x: "time", y: "linear" }}
+				data={chartData}
+			/>
+			<VictoryScatter size={4} style={{ data: { fill: "white", stroke: colorTheme.accent, strokeWidth: 1 } }} data={chartData} />
+		</VictoryChart>
+	);
 }
 
 export default CurrentWeekChart;
--- src/screens/HomeScreen/components/Footer.js
+++ src/screens/HomeScreen/components/Footer.js
@@ -1,42 +1,14 @@
 import React from "react";
-import { Text, View, Dimensions } from "react-native";
+import { Text, View } from "react-native";
 import DataItem from "./DataItem";
 import moment from "moment";
 import tw from "twrnc";
 import DateChangeTabs from "../../../components/DateChangeTabs";
-import { VictoryChart, VictoryArea, VictoryScatter, VictoryAxis, VictoryTheme } from "victory-native";
-import { colorTheme } from "../../../constants/styles";
+import CurrentWeekChart from "./CurrentWeekChart";
 
 function Footer({ week: { average, delta, data, startDate, endDate }, hide, goal, handleDateChange }) {
 	let remaining = goal ? (goal - average).toFixed(2).toString() : "--";
-	const windowWidth = Dimensions.get("window").width;
-	const windowHeight = Dimensions.get("window").height * 0.25;
-	let yMax = 215;
-	let yMin = 200;
 
-	const enumerateDaysBetweenDates = (startDate, endDate) => {
-		let dates = [];
-
-		while (moment(startDate) <= moment(endDate)) {
-			dates.push(startDate);
-			startDate = moment(startDate).add(1, "days").format("YYYY-MM-DD");
-		}
-
-		return dates;
-	};
-
-	let currentWeek = enumerateDaysBetweenDates(startDate, endDate);
-
-	let chartData = data
-		.sort((a, b) => new Date(a.date) - new Date(b.date))
-		.map((entry, i) => {
-			if (parseFloat(entry.weight) < yMin) yMin = parseFloat(entry.weight);
-			if (parseFloat(entry.weight) > yMax) yMax = parseFloat(entry.weight);
-			return { x: moment(entry.date).format("YYYY-MM-DD"), y: parseFloat(entry.weight), key: i };
-		});
-
-	console.log("Current Week", currentWeek);
-	console.log("Data", chartData);
 	return (
 		<View style={tw.style("flex-col", hide && "opacity-0")}>
 			<DateChangeTabs date={startDate} handleDateChange={handleDateChange} dateUnit={"w"} size={18} padding={"mx-12"}>
@@ -50,32 +22,7 @@ function Footer({ week: { average, delta, data, startDate, endDate }, hide, goal
 				<DataItem icon="bullseye" int={remaining} type={remaining === undefined ? "numeric" : "change"} />
 			</View>
 			<View style={tw.style("flex flex-row")}>
-				<VictoryChart width={windowWidth} height={windowHeight} theme={VictoryTheme.material} domain={{ y: [yMin - 5, yMax + 5] }}>
-					<VictoryAxis
-						dependentAxis
-						maxDomain={{ y: 5 }}
-						style={{
-							grid: { stroke: "white", strokeWidth: 0 },
-							ticks: { stroke: "white", strokeWidth: 0 },
-						}}
-					/>
-					<VictoryAxis
-						tickValues={currentWeek}
-						maxDomain={{ x: 7 }}
-						style={{
-							grid: { stroke: "lightgrey", strokeWidth: 1 },
-							ticks: { stroke: "white", strokeWidth: 0 },
-						}}
-						tickFormat={(t) => `${moment(t).format("ddd")}\n${moment(t).format("D")}`}
-					/>
-					<VictoryArea
-						interpolation="monotoneX"
-						style={{ data: { stroke: colorTheme.accent, strokeWidth: 2, fill: colorTheme.accent, fillOpacity: 0.25 } }}
-						scale={{ x: "time", y: "linear" }}
-						data={chartData}
-					/>
-					<VictoryScatter size={4} style={{ data: { fill: "white", stroke: colorTheme.accent, strokeWidth: 1 } }} data={chartData} />
-				</VictoryChart>
+				<CurrentWeekChart startDate={startDate} endDate={endDate} data={data} />
 			</View>
 		</View>
 	);
